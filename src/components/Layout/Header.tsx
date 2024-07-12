b29ce5a3a2dae9f3135fe4edb87a6e61d2b0b60a
Franklin J. Valdez De Los Santos <fjvaldez@ind.local>
2024-07-12 12:26:37 -0400

    Improved menu header design :D

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,79 +1,188 @@
 import React, { useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button, Box, Avatar, Menu, MenuItem } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Box, Avatar, Menu, MenuItem, IconButton, Tooltip,Container } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import ProtestIcon from '@mui/icons-material/Gavel';
+import LeaderIcon from '@mui/icons-material/Person';
+import NatureIcon from '@mui/icons-material/Nature';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { useAuth } from '../../hooks/useAuth';
 
 const Header: React.FC = () => {
   const { user, logout } = useAuth();
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
 
-  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
   };
 
   const handleLogout = () => {
     logout();
-    handleClose();
+    handleCloseUserMenu();
   };
 
+  const menuItems = user ? [
+    { text: 'Protestas', path: '/protestas', icon: <ProtestIcon /> },
+    { text: 'Cabecillas', path: '/cabecillas', icon: <LeaderIcon /> },
+    { text: 'Naturalezas', path: '/naturalezas', icon: <NatureIcon /> },
+  ] : [];
+
   return (
-    <AppBar position="static" color="primary" elevation={0}>
-      <Toolbar>
-        <Typography variant="h6" component={RouterLink} to="/" sx={{ flexGrow: 1, textDecoration: 'none', color: 'inherit' }}>
-          Protestas App
-        </Typography>
-        <Box>
-          {user ? (
-            <>
-              <Button color="inherit" component={RouterLink} to="/protestas" sx={{ mr: 1 }}>
-                Protestas
-              </Button>
-              <Button color="inherit" component={RouterLink} to="/cabecillas" sx={{ mr: 1 }}>
-                Cabecillas
-              </Button>
-              <Button color="inherit" component={RouterLink} to="/naturalezas" sx={{ mr: 1 }}>
-                Naturalezas
-              </Button>
-              <Avatar
-                alt={`${user.nombre} ${user.apellidos}`}
-                src={user.foto}
-                sx={{ cursor: 'pointer' }}
-                onClick={handleMenu}
-              />
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <ProtestIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component={RouterLink}
+            to="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            PROTESTAS
+          </Typography>
+
+          {user && (
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
               <Menu
                 id="menu-appbar"
-                anchorEl={anchorEl}
+                anchorEl={anchorElNav}
                 anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: 'bottom',
+                  horizontal: 'left',
                 }}
                 keepMounted
                 transformOrigin={{
                   vertical: 'top',
-                  horizontal: 'right',
+                  horizontal: 'left',
                 }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
               >
-                <MenuItem onClick={handleClose}>Perfil</MenuItem>
-                <MenuItem onClick={handleLogout}>Salir</MenuItem>
+                {menuItems.map((item) => (
+                  <MenuItem key={item.text} onClick={handleCloseNavMenu} component={RouterLink} to={item.path}>
+                    {item.icon}
+                    <Typography textAlign="center" sx={{ ml: 1 }}>{item.text}</Typography>
+                  </MenuItem>
+                ))}
               </Menu>
-            </>
-          ) : (
-            <>
-              <Button color="inherit" component={RouterLink} to="/login" sx={{ mr: 1 }}>
-                Iniciar sesión              </Button>
-              <Button color="secondary" variant="contained" component={RouterLink} to="/register">
-                Registrarse
-              </Button>
-            </>
+            </Box>
           )}
-        </Box>
-      </Toolbar>
+          <ProtestIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component={RouterLink}
+            to="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            PROTESTAS
+          </Typography>
+          {user && (
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {menuItems.map((item) => (
+                <Button
+                  key={item.text}
+                  component={RouterLink}
+                  to={item.path}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'flex', alignItems: 'center' }}
+                >
+                  {item.icon}
+                  <span style={{ marginLeft: '8px' }}>{item.text}</span>
+                </Button>
+              ))}
+            </Box>
+          )}
+
+          <Box sx={{ flexGrow: 0 }}>
+            {user ? (
+              <>
+                <Tooltip title="Abrir opciones">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar alt={`${user.nombre} ${user.apellidos}`} src={user.foto} />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: '45px' }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  <MenuItem onClick={handleCloseUserMenu} component={RouterLink} to="/perfil">
+                    <AccountCircleIcon sx={{ mr: 1 }} />
+                    <Typography textAlign="center">Perfil</Typography>
+                  </MenuItem>
+                  <MenuItem onClick={handleLogout}>
+                    <Typography textAlign="center">Salir</Typography>
+                  </MenuItem>
+                </Menu>
+              </>
+            ) : (
+              <>
+                <Button color="inherit" component={RouterLink} to="/login" sx={{ mr: 1 }}>
+                  Iniciar sesión
+                </Button>
+                <Button color="secondary" variant="contained" component={RouterLink} to="/register">
+                  Registrarse
+                </Button>
+              </>
+            )}
+          </Box>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 };
