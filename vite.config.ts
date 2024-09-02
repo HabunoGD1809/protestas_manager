071545014c7b183e462a31a70d5abba1c9181cb3
Habunin :D <franklinjoel1821@gmail.com>
2024-09-02 16:08:17 -0400

    update viteConfig v3

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', 
-    port: 9010, 
+    // port: 9010, 
   },
   build: {
     outDir: 'dist',
-  },
-  define: {
-    'process.env.VITE_API_URL': JSON.stringify('http://10.5.5.18:8000'),
   },
 })
