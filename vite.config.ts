a3e08d9655232a110ec47ab3e56ca2b7a0ead889
Franklin J. Valdez De Los Santos <fjvaldez@ind.local>
2024-09-03 15:40:26 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', 
-    port: 9010, 
+    host: '0.0.0.0',
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://10.5.5.18:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
   build: {
     outDir: 'dist',
